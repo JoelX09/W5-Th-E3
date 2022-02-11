d53bf49424007cac2963145bac78937539397c71
JoelX09 <3002421820101@ingenieria.usac.edu.gt>
2022-02-11 14:20:16 -0600

    Update E3

---
+++ src/Rational.ts
@@ -0,0 +1,47 @@
+export class Rational {
+  private numerator: number;
+  private denominator: number;
+
+  constructor(numerator: number, denominator: number) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+
+  printRational() {
+    console.log('==================');
+    console.log(`Operation: ${this.numerator} / ${this.denominator}`);
+    console.log('==================');
+  }
+
+  invert() {
+    let temp = this.numerator;
+    this.numerator = this.denominator;
+    this.denominator = temp;
+  }
+
+  toFloat() {
+    return this.numerator / this.denominator;
+  }
+
+  reduce() {
+    let A = this.numerator;
+    let B = this.denominator;
+    let R = 0;
+
+    while (A !== 0 && B !== 0) {
+        R=A%B;
+        A = B;
+        B = R;
+    }
+
+    if(A === 0){
+        this.numerator /= B;
+        this.denominator /= B;
+    }
+
+    if(B === 0){
+        this.numerator /= A;
+        this.denominator /= A;
+    }
+  }
+}
